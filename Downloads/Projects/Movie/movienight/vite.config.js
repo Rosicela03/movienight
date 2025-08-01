fd1420c4de901be03d798123a2544af25ba99dd2
Rosieluna <rluna@drew.edu>
2025-08-01 11:46:38 -0400

    Set base path for GitHub Pages deployment

--- Downloads/Projects/Movie/movienight/vite.config.js
+++ Downloads/Projects/Movie/movienight/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
+
+  //repo name 
+  base: '/movienight/',
+
   plugins: [ react(), svgr()],
 });
